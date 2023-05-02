37382b29edb839a01c319207ed55ccd7d2bcf297
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-02 16:07:47 +0600

    35 template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular14';
+  title = 'Template Driven Form';
+
+  userLogin(item: any) {
+    console.warn(item);
+  }
 }
