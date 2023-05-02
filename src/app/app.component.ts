c69dcb8bff44445be5673cc7b906aa6fc11fbc94
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-02 15:50:39 +0600

    20 style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular14';
+  title = 'Style Binding';
+  color = 'red';
+  bgColor='green';
+
+  updateColor(){
+    this.color = 'white';
+    this.bgColor="black";
+  }
 }
