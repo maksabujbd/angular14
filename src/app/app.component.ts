dd7f06601c8e951e21ff8e112b6aabaf161ccbdc
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-02 16:28:23 +0600

    36 reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular14';
+  title = 'Angular Reactive Form';
+  loginForm = new FormGroup({
+    name: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  userLogin(item: any) {
+    console.warn(item);
+  }
+
+  loginUser() {
+    console.warn(this.loginForm.value);
+  }
 }
